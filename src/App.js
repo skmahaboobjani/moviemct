4036c744530978129624eb385b0a0729a89de0e8
mahaboob jani <skmabujani521@gmail.com>
2022-11-10 21:30:51 +0530

    mahaboob

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useState} from 'react'
+import "./App.css"
+const App = () => {
+  const [search,setSearch] = useState('');
+  const [data,setData] = useState([]);
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`http://www.omdbapi.com/?s=${search}&apikey=89a8c783`).then(
+      response => response.json()
+    ).then(value => {
+      setData(value.Search);
+    })
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <center>
+          <h1>Search Your Favorite Movie</h1>
+          <form onSubmit={submitHandler}>
+            <input  style={{width:"300px",height:'20px',position:'relative',bottom:"10px"}} type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            <input type="submit" value="Search" style={{position:'relative',bottom:"10px"}}  />
+          </form>
+          <div  >
+          {data.length>=1?data.map(movie=>
+          <div key={movie.imdbID}>
+            <div className="cards" style={{"width": "18rem"}}>
+              <img src={movie.Poster}  alt={movie.Title} />
+              <div >
+                <h4 >{movie.Title}</h4>
+                <p>{movie.Year}</p>
+                
+              </div>
+            </div>
+          </div>
+            ):null}
+            </div>
+        </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
